ed13109210efd4792ddf095604b472174d95e8e9
Mrcode <734202639@qq.com>
2016-06-24 13:58:21 +0800

    server

--- server/routes.js
+++ server/routes.js
@@ -1,3 +1,19 @@
 /**
  * Created by mrcode on 16-6-21.
  */
+
+let express = require('express'),
+    router  = express.Router()
+    path    = require('path')
+
+router.use(function timeLog(req, res, next) {
+    console.log('Time: ', Date.now());
+    next();
+});
+
+router.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '../dist/index.html')));
+
+router.get('/test', (req, res) => res.send('hello world'));
+
+module.exports = router;
